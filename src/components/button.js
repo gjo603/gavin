377913289b341a3e2ba0ad56fd12c3c1406c1b75
Jordan Hudgens <jordan@devcamp.com>
2018-07-18 12:49:14 -0600

    created funcional button

---
+++ src/components/button.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+const Button = () => {
+    return (
+        <button className="button">
+            Generate Countdown
+        </button>
+    )
+}
+
+export default Button;
